63a68c0d3d18bf4794dab48cf18146431f8ded5e
andrerieger <49887545+andrerieger@users.noreply.github.com>
2026-01-12 11:17:10 -0400

    outra tentativa de corrigir o bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  // We use '.' instead of process.cwd() to avoid TS errors in some environments
   const env = loadEnv(mode, '.', '');
 
   return {
